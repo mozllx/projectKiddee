d6e5c886bff0837e11c7d8259ee989c9b32c0f4a
kanyawee2540 <kanyawi.p@gmail.com>
2021-03-29 02:47:22 +0700

    add sorry1

---
+++ Assets/SPRITES/speking/InGameControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameControl : MonoBehaviour
+{
+    [SerializeField]
+    public AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void playSoundEffect()
+    {
+        sound.Play();
+    }
+
+}
---
+++ Assets/SPRITES/speking/canvas2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canvas2 : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 100000f;
+    [SerializeField]
+    private float timeElapsed;
+    public GameObject turtleWalkUI;
+    public GameObject rabbitRunUI;
+    public GameObject crashUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        turtleWalkUI.SetActive(true);
+        rabbitRunUI.SetActive(false);
+        crashUI.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > delayBeforeLoading)
+        {
+            turtleWalkUI.SetActive(false);
+            rabbitRunUI.SetActive(true);
+            crashUI.SetActive(false);
+        }
+           
+
+    }
+}
