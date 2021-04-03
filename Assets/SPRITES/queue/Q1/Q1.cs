687e4da47e9996a5a31060c457f01080cf2ae61e
kanyawee2540 <kanyawi.p@gmail.com>
2021-04-04 02:47:42 +0700

    update queue

---
+++ Assets/SPRITES/queue/Q1/Q1.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Q1 : MonoBehaviour
+{
+    public GameObject correctUI;
+    public GameObject inCorrectUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        correctUI.SetActive(false);
+        inCorrectUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragS1.locked)
+        {
+            correctUI.SetActive(true);
+        }
+        if(dragS2.locked )
+        {
+            correctUI.SetActive(false);
+            inCorrectUI.SetActive(true);
+        }/*
+        if (dragS3.locked )
+        {
+            correctUI.SetActive(false);
+            inCorrectUI.SetActive(true);
+        }
+        if (dragS4.locked)
+        {
+            correctUI.SetActive(false);
+            inCorrectUI.SetActive(true);
+        }*/
+        /*if (hamburger.locked && watermalon.locked && unicon.locked && tomato.locked)
+        {
+            winText1.SetActive(true);
+        }
+        if (trash.locked && trash2.locked && trash3.locked && trash4.locked && trash5.locked && trash6.locked)
+        {
+            winText3.SetActive(true);
+        }*/
+
+    }
+    public void goToQ2()
+    {
+        SceneManager.LoadScene("Q2");
+    }
+}
---
+++ Assets/SPRITES/queue/Q2/Q2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Q2 : MonoBehaviour
+{
+    public GameObject correctUI;
+    public GameObject inCorrectUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        correctUI.SetActive(false);
+        inCorrectUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Q2DragS2.locked)
+        {
+            correctUI.SetActive(true);
+        }
+        if (Q2DragS1.locked || Q2DragS3.locked || Q2DragS4.locked)
+        {
+            correctUI.SetActive(false);
+            inCorrectUI.SetActive(true);
+        }
+        /*if (hamburger.locked && watermalon.locked && unicon.locked && tomato.locked)
+        {
+            winText1.SetActive(true);
+        }
+        if (trash.locked && trash2.locked && trash3.locked && trash4.locked && trash5.locked && trash6.locked)
+        {
+            winText3.SetActive(true);
+        }*/
+
+    }
+    /*public void goToQ2()
+    {
+        SceneManager.LoadScene("Q2");
+    }*/
+}
