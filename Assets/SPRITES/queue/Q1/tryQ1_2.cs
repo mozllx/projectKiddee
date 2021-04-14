3a3eede5c9fcc5210d6af9a0559a713c1d19a13b
kanyawee2540 <kanyawi.p@gmail.com>
2021-04-14 22:52:35 +0700

    update queue

---
+++ Assets/SPRITES/queue/Q1/tryQ1_2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tryQ1_2 : MonoBehaviour
+{
+    public GameObject questionUI;
+    public GameObject correctUI;
+    public GameObject inCorrectUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        questionUI.SetActive(true);
+        correctUI.SetActive(false);
+        inCorrectUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Q1DragS1.locked)
+        {
+            questionUI.SetActive(false);
+            correctUI.SetActive(true);
+        }
+        if (Q1DragS2.locked || Q1DragS3.locked || Q1DragS4.locked)
+        {
+            questionUI.SetActive(false);
+            inCorrectUI.SetActive(true);
+            Q1DragS2.locked = false;
+            Q1DragS3.locked = false;
+            Q1DragS4.locked = false;
+
+        }
+
+    }
+    public void goToQ2()
+    {
+        SceneManager.LoadScene("Q2");
+    }
+    public void tryQ1Again3()
+    {
+        SceneManager.LoadScene("TryQ1Again");
+    }
+}
